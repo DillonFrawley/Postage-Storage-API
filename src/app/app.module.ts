8aff7fd9e0495de12db1df486132993f86d7bc73
Dillon Frawley <dfra0004@student.monash.edu>
2022-10-04 11:55:47 +1100

    need to add pwa still

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,9 +45,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately',
     })
   ],
   providers: [],
